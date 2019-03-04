78c130598fe8e3ac912ae8d1b14beed8540baa48
Huiying0108 <whymissu1228@gmail.com>
2019-03-04 08:32:32 -0800

    Updates

---
+++ precache-manifest.c441e0badc3dfdb0682ff73228bda99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e9f23ebaf2f0f798584",
+    "url": "/reactjs-homework/static/js/runtime~main.5e9f23eb.js"
+  },
+  {
+    "revision": "04df92bbe706a5e28db1",
+    "url": "/reactjs-homework/static/js/main.04df92bb.chunk.js"
+  },
+  {
+    "revision": "00ec0959a47ed983999e",
+    "url": "/reactjs-homework/static/js/2.00ec0959.chunk.js"
+  },
+  {
+    "revision": "04df92bbe706a5e28db1",
+    "url": "/reactjs-homework/static/css/main.36dbbb1c.chunk.css"
+  },
+  {
+    "revision": "63bebfaecc62c97e1fda563f3030b3e3",
+    "url": "/reactjs-homework/index.html"
+  }
+];
